645bdb2301c98386849d00f2836153f040853259
winning godspower <winninggodspower@gmail.com>
2022-06-07 02:16:54 +0100

    innitial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: 'hsl(172, 67%, 45%)',
+        'dark-cyan': 'hsl(183, 100%, 15%)',
+        'light-cyan': 'hsl(185, 41%, 84%)',
+        'gray-cyan': 'hsl(184, 14%, 56%)',
+
+        white: 'hsl(0, 0%, 100%)',
+      }
+    },
+  },
+  plugins: [],
+}
